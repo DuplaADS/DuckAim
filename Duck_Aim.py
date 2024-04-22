7eb706793df00f2e2802b93da0edf97b24196362
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-04-21 23:19:16 -0300

    Mudança na paleta de cores(patos e textos)

--- Duck_Aim.py
+++ Duck_Aim.py
@@ -14,20 +14,20 @@ comicsans = pygame.font.SysFont("comicsans", 50)
 comicsansMenor = pygame.font.SysFont("comicsans", 30)
 corBranca = (255, 255, 255)
 corAmarela = (250, 241, 110) 
-corAzul = (22, 206, 247)
+corRoxa = (210, 145, 250)
 
 vida_amarelo = 3
-vida_azul = 3
+vida_roxo = 3
 
 
 # ----Gravidade----
 gravidade_amarelo = 0
-gravidade_azul = 0
+gravidade_roxo = 0
 
 
 # ----Sobre_Tiros-----
 balas_amarelas = []
-balas_azuis = []
+balas_roxas = []
 
 # --- SONS ---
 som_atirando = pygame.mixer.Sound("sons/shoot.wav")
@@ -39,30 +39,30 @@ pygame.display.set_caption("Duck Aim")
 
 background = pygame.image.load("imagens/background.jpg")
 amarelo = pygame.image.load("imagens/amarelo.png")
-azul = pygame.image.load("imagens/azul.png")
+roxo = pygame.image.load("imagens/roxo.png")
 tiro_amarelo = pygame.image.load("imagens/tiro_amarelo.png")
-tiro_azul = pygame.image.load("imagens/tiro_azul.png")
+tiro_roxo = pygame.image.load("imagens/tiro_roxo.png")
 
 # -----Retangulos (HitBox)----
 rect_amarelo = pygame.Rect(700, 631, 40, 64)
-rect_azul = pygame.Rect(40, 631, 40, 64)
+rect_roxo = pygame.Rect(40, 631, 40, 64)
 
 # Funções:
 
-def desenhar(tiros_amarelos, tiros_azuis, disparo_am, disparo_az, hp1, hp2):
+def desenhar(tiros_amarelos, tiros_roxos, disparo_am, disparo_roxo, hp1, hp2):
     janela.blit(background, (0, 0))
     janela.blit(amarelo, rect_amarelo)
-    janela.blit(azul, rect_azul)
+    janela.blit(roxo, rect_roxo)
 
     for balas in tiros_amarelos:
         janela.blit(disparo_am, balas)
-    for balas in tiros_azuis:
-        janela.blit(disparo_az, balas)
+    for balas in tiros_roxos:
+        janela.blit(disparo_roxo, balas)
 
-    txt_vida_amarelo = comicsans.render("HP: " + str(hp1), 1, corAmarela)
-    txt_vida_azul = comicsans.render("HP: " + str(hp2), 1, corAzul)
+    txt_vida_amarelo = comicsans.render("HP: " + str(hp1), 1, corBranca)
+    txt_vida_roxo = comicsans.render("HP: " + str(hp2), 1, corBranca)
     janela.blit(txt_vida_amarelo, (670, 0))
-    janela.blit(txt_vida_azul, (0, 0))
+    janela.blit(txt_vida_roxo, (0, 0))
 
 # ---- Player ----
 def player(rect, left, right, down):
@@ -79,7 +79,7 @@ def player(rect, left, right, down):
 
 
 VelAmarelas = -12
-VelAzuis = 12
+VelRoxas = 12
 # ---- Atirando ----
 def atirar(listatiros, vel, inimigo, hp_inimigo):
 
@@ -105,43 +105,44 @@ while True:
 
         elif event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_UP and rect_amarelo.bottom == 567:
+            if event.key == pygame.K_UP and rect_amarelo.bottom == 567 and vida_amarelo !=0:
                 som_pulando.play()                
                 gravidade_amarelo = -20
 
-            if event.key == pygame.K_w and rect_azul.bottom == 567:
+            if event.key == pygame.K_w and rect_roxo.bottom == 567 and vida_roxo != 0:
+                
                 som_pulando.play()
-                gravidade_azul = -20
+                gravidade_roxo = -20
 
-            if event.key == pygame.K_RSHIFT and len(balas_amarelas) < 2:
+            if event.key == pygame.K_RSHIFT and len(balas_amarelas) < 2  and vida_amarelo != 0:
                 som_atirando.play()
                 bala = pygame.Rect(rect_amarelo.x, rect_amarelo.y + 32, 15, 8)
                 balas_amarelas.append(bala)
 
-            if event.key == pygame.K_LSHIFT and len(balas_azuis) < 2:
+            if event.key == pygame.K_LSHIFT and len(balas_roxas) < 2 and vida_roxo != 0:
                 som_atirando.play()
-                bala = pygame.Rect(rect_azul.x + 32, rect_azul.y + 32, 15, 8)
-                balas_azuis.append(bala)
+                bala = pygame.Rect(rect_roxo.x + 32, rect_roxo.y + 32, 15, 8)
+                balas_roxas.append(bala)
 
     # Parte Visual
-    desenhar(balas_amarelas, balas_azuis, tiro_amarelo, tiro_azul, vida_amarelo, vida_azul)
+    desenhar(balas_amarelas, balas_roxas, tiro_amarelo, tiro_roxo, vida_amarelo, vida_roxo)
 
 # Current Game
-    if vida_amarelo != 0 and vida_azul != 0:
+    if vida_amarelo != 0 and vida_roxo != 0:
 
         # Players (Gravidade e Movimentos)
         gravidade_amarelo += 1
-        gravidade_azul += 1
+        gravidade_roxo += 1
         rect_amarelo.y += gravidade_amarelo
-        rect_azul.y += gravidade_azul
+        rect_roxo.y += gravidade_roxo
 
         # Players
         player(rect_amarelo, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN)
-        player(rect_azul, pygame.K_a, pygame.K_d, pygame.K_s)
+        player(rect_roxo, pygame.K_a, pygame.K_d, pygame.K_s)
 
         # Atirando
-        vida_azul = atirar(balas_amarelas, VelAmarelas, rect_azul, vida_azul)
-        vida_amarelo = atirar(balas_azuis, VelAzuis, rect_amarelo, vida_amarelo)
+        vida_roxo = atirar(balas_amarelas, VelAmarelas, rect_roxo, vida_roxo)
+        vida_amarelo = atirar(balas_roxas, VelRoxas, rect_amarelo, vida_amarelo)
 
         # Barreira (pato)
         if rect_amarelo.left <= 400:
@@ -149,33 +150,33 @@ while True:
         elif rect_amarelo.right >= 800:
             rect_amarelo.right = 800
         # Barreira (pata)
-        if rect_azul.right >= 400:
-            rect_azul.right = 400
-        elif rect_azul.left <= 0:
-            rect_azul.left = 0
+        if rect_roxo.right >= 400:
+            rect_roxo.right = 400
+        elif rect_roxo.left <= 0:
+            rect_roxo.left = 0
 
 
     # GameOver
     else:
         if vida_amarelo == 0:
-            game_over = comicsans.render("Azul venceu", 1, corAzul)
-            janela.blit(game_over, (270, 200))
-        elif vida_azul == 0:
+            game_over = comicsans.render("Roxo   venceu", 1, corRoxa)
+            janela.blit(game_over, (260, 0))
+        elif vida_roxo == 0:
             game_over = comicsans.render("Amarelo Venceu", 1, corAmarela)
-            janela.blit(game_over, (200, 200))
+            janela.blit(game_over, (200, 0))
         
         #Restaurando jogo
         
         balas_amarelas.clear()
-        balas_azuis.clear()
+        balas_roxas.clear()
 
         restart = comicsansMenor.render("Reiniciar? (R)", 1, corBranca)
 
-        janela.blit(restart, (310, 300))
+        janela.blit(restart, (320, 300))
         if pygame.key.get_pressed()[pygame.K_r]:
 
             rect_amarelo = pygame.Rect(700, 631, 40, 64)
-            rect_azul = pygame.Rect(40, 631, 40, 64)
+            rect_roxo = pygame.Rect(40, 631, 40, 64)
             vida_amarelo = 3
-            vida_azul = 3
+            vida_roxo = 3
 
